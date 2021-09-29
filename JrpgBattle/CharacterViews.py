90098266c78fd91e82f6c9d79af35907f698235b
unfallible <ryn292@mocs.utc.edu>
2021-09-28 22:48:27 -0500

    Initial commmit, containing non-functioning battle engine

---
+++ JrpgBattle/CharacterViews.py
@@ -0,0 +1,66 @@
+from typing import Set, TYPE_CHECKING
+from fractions import Fraction
+from copy import copy
+from JrpgBattle.Character import CharacterStatus, Multiplier
+if TYPE_CHECKING:
+    from JrpgBattle.Attack import Attack
+    from JrpgBattle.Party import Party
+
+
+class PublicCharacterView:
+    """
+    PublicCharacterView objects provide views of CharacterStatus objects
+    without methods for easily modifying them. Its interface also provides
+    methods which only allow access to the features of the viewed object
+    which are viewable by all players. This view is mainly intended to be
+    sent to PlayerServer objects to allow them to keep track of the game state
+    and send orders to the BattleClient. The PublicCharacterView is the most
+    restrictive view. However, it can be extended to create more permissive
+    views as well.
+    """
+    def __int__(self, character: CharacterStatus):
+        self._character = character
+
+    def get_name(self) -> str:
+        return self._character.get_name()
+
+    def get_party(self) -> Party:
+        return self._character.party
+
+    def get_vulnerability(self) -> int:
+        return self._character.get_vulnerability()
+
+    def get_damage(self) -> int:
+        return self._character.get_max_hp() - self._character.get_current_hp()
+
+    def get_sp(self) -> int:
+        return self._character.current_sp
+
+    def get_sp_spent(self) -> int:
+        return self._character.sp_spent
+
+    def get_ap(self) -> int:
+        return self._character.current_ap
+
+    def get_public_attack_list(self) -> Set[Attack]:
+        return copy(self._character.get_public_attack_list())
+
+    def get_public_offensive_multipliers(self) -> Set[Multiplier]:
+        return self._character.public_offensive_multipliers
+
+    def get_public_defensive_multipliers(self) -> Set[Multiplier]:
+        return self._character.public_defensive_multipliers
+
+    def was_attacked(self) -> bool:
+        return self._character.was_attacked
+
+    def is_staggered(self) -> bool:
+        return self._character.stagger
+
+    def is_dead(self) -> bool:
+        return self._character.is_dead()
+
+
+class PrivateCharacterView(PublicCharacterView):
+    def __init__(self, character: CharacterStatus):
+        super.__init__(character)
